34b3c2a42a22da2a8a3c3e0c89ddd58270620a1d
Esuabom Dijemeni <e.dijemeni@gmail.com>
2021-12-23 23:30:21 +0000

    Completed top card and custom colors

--- bill_splitter/lib/ui/home.dart
+++ bill_splitter/lib/ui/home.dart
@@ -1,3 +1,4 @@
+import 'package:bill_splitter/util/hexcolor.dart';
 import 'package:flutter/material.dart';
 
 class BillSplitter extends StatefulWidget {
@@ -12,13 +13,15 @@ class _BillSplitterState extends State<BillSplitter> {
   int _personCounter = 1;
   double _billAmount = 0.0;
 
+  Color _purle = HexColor("#6908D6");
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         margin: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.1),
         alignment: Alignment.center,
-        color: Colors.greenAccent,
+        color: Colors.transparent,
         child: ListView(
           scrollDirection: Axis.vertical,
           padding: EdgeInsets.all(20.5),
@@ -27,17 +30,47 @@ class _BillSplitterState extends State<BillSplitter> {
               width: 150,
               height: 150,
               decoration: BoxDecoration(
-                  color: Colors.purpleAccent.shade100,
+                  color: _purle.withOpacity(0.1),
                   borderRadius: BorderRadius.circular(12.0)),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text("Total Per Person"),
-                  Text("\$50.0")
-                ],
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[Text("Total Per Person"), Text("\$50.0")],
+                ),
               ),
             ),
-            Container()
+            Container(
+              margin: EdgeInsets.only(top:20.0),
+              padding: EdgeInsets.all(12.0),
+              decoration: BoxDecoration(
+                color: Colors.transparent,
+                border: Border.all(
+                  color: Colors.blueGrey.shade100,
+                  style: BorderStyle.solid),
+                borderRadius: BorderRadius.circular(12.0)
+
+              ),
+              child: Column(
+                children: <Widget>[
+                  TextField(
+                    keyboardType: TextInputType.numberWithOptions(decimal: true),
+                    style: TextStyle(color: Colors.purple.shade500),
+                    decoration: InputDecoration(
+                      prefixText: "Bill Amount",
+                      prefixIcon: Icon(Icons.attach_money)
+                    ),
+                    onChanged: (String value){
+                      try{
+                        _billAmount = double.parse(value);
+                      } catch (exception){
+                        _billAmount = 0.0;
+                      }
+                    }
+                  )
+                ],
+              ),
+
+            )
           ],
         ),
       ),
